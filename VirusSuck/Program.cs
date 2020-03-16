06742fedd4acb3e4e36cd583d2491933d4422b0d
Jamie <phillipsja97@gmail.com>
2020-03-16 18:26:24 -0500

    Add project files.

---
+++ VirusSuck/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VirusSuck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a new list
+            var names = new List<string>();
+
+            // add to that list by variable type 
+            names.Add("COVID-19");
+            names.Add("Ebola");
+            names.Add("Spanish Flu");
+            names.Add("SARS");
+            names.Add("Rabies");
+            names.Add("Stupidity");
+
+            // remove an item by name
+            names.Remove("Stupidity");
+            // remove an item by index
+            names.RemoveAt(3);
+
+
+        }
+    }
+}
